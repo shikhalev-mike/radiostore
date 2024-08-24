6549900bd0678c6382129f28ec540998f0dd0268
Mikhail Shikhalev <shikhalev.mike@yandex.ru>
2024-08-24 14:33:31 +0400

    Initial commit

---
+++ src/main/java/com/company/radiostore/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.radiostore.view.user;
+
+import com.company.radiostore.entity.User;
+import com.company.radiostore.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
